5ed26910bd3a211a5c33179434732a626f6024c7
365940875 <365940875@qq.com>
2021-11-09 21:11:08 +0800

    offer big breast

---
+++ OFFERS/BIGBREAST/tz.js
@@ -0,0 +1,58 @@
+/*璺宠浆寮€濮�*/
+
+//$(function() {
+//	$('.ClassA').on('copy', function() {
+//		console.log(1);
+//		window.location.href = goToUrl();
+//	});
+//	$('.ToUrl').on('click', function() {
+//		console.log(1);
+//		window.location.href = goToUrl();
+//	});
+//	$('.leftTc').on('click', function() {
+//		console.log(1);
+//		window.location.href = goToUrl();
+//	});
+//	$('#ClassD').on('copy', function() {
+//		console.log(1);
+//		window.location.href = goToUrl();
+//	});
+//
+//})
+//var goToUrl = function() {
+//	var url = [
+//		'http://jcmy0755.com/go.php?id=1056&sign=cBDkZW',
+//	];
+//
+//	return url[Math.floor(Math.random() * url.length)];
+//}
+
+/**寮圭獥寮€濮�*/
+
+
+
+
+$(function(){
+    $('.leftTc').on('click',function(){
+       $('#tanchuang').show();
+    })
+}) 
+$(function(){
+    $('.ToUrl').on('click',function(){
+       $('#tanchuang').show();
+    })
+})
+
+$(function() {
+    $('#ClassD').on('', function() {
+             $('#tanchuang').show();
+    });
+})
+
+$(function() {
+    $('.ClassA').on('copy', function() {
+             $('#tanchuang').show();
+    });
+})
+
+/*寮圭獥缁撴潫*/
